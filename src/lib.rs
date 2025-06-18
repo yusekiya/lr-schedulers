a2fc6171b62a2df5580c91d0cbe39c299455eca1
yusekiya <y.seki.14870@gmail.com>
2025-06-18 12:40:16 +0900

    Improve doc comment for Scheduler trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 pub mod constant;
-pub mod linear;
-pub mod exponential;
 pub mod cosine_annealing;
 pub mod cosine_annealing_warm_restarts;
+pub mod exponential;
+pub mod linear;
 
 pub trait Scheduler {
     /// Proceeds the step of scheduler.
     fn step(&mut self, loss: f64);
     /// Returns a learning rate for the current step.
-    fn get_lr(&self, loss: f64) -> f64; // The argument `loss` is for schedulers such as ReduceLROnPlateau.
+    ///
+    /// The argument `loss` is for schedulers that require history of loss value such as ReduceLROnPlateau.
+    fn get_lr(&self, loss: f64) -> f64;
 }
+
